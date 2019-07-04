02f88e85c16c4f11f6dfed5d3f0b25b322121ca5
Troy Liu <troyliu0105@outlook.com>
2019-07-04 20:09:55 +0800

    UPDATE: support for OpenCV4

--- synthblur/src/video.cpp
+++ synthblur/src/video.cpp
@@ -2,6 +2,17 @@
 #include <iostream>
 #include "video.h"
 
+#if CV_MAJOR_VERSION >= 4
+#include <opencv2/videoio.hpp>
+#define CV_CAP_PROP_POS_FRAMES    cv::VideoCaptureProperties::CAP_PROP_POS_FRAMES
+#define CV_CAP_PROP_FPS           cv::VideoCaptureProperties::CAP_PROP_FPS
+#define CV_CAP_PROP_FRAME_WIDTH   cv::VideoCaptureProperties::CAP_PROP_FRAME_WIDTH
+#define CV_CAP_PROP_FRAME_HEIGHT  cv::VideoCaptureProperties::CAP_PROP_FRAME_HEIGHT
+#define CV_CAP_PROP_FRAME_COUNT   cv::VideoCaptureProperties::CAP_PROP_FRAME_COUNT
+#define CV_CAP_PROP_POS_FRAMES    cv::VideoCaptureProperties::CAP_PROP_POS_FRAMES
+#define CV_FOURCC                 cv::VideoWriter::fourcc
+#endif
+
 VideoReader::VideoReader(std::string fn) : hnd(fn), path(fn) {
   std::cout << "read video " << fn << std::endl
             << "  fps:    " << fps() << std::endl
@@ -16,18 +27,16 @@ void VideoReader::jump(unsigned int idx) {
 
 }
 
-double VideoReader::fps() const { return hnd.get(CV_CAP_PROP_FPS);}
-unsigned int  VideoReader::width() const { return hnd.get(CV_CAP_PROP_FRAME_WIDTH);}
-unsigned int  VideoReader::height() const { return hnd.get(CV_CAP_PROP_FRAME_HEIGHT);}
-unsigned int VideoReader::frames() const { return hnd.get(CV_CAP_PROP_FRAME_COUNT);}
-unsigned int VideoReader::frame() const { return hnd.get(CV_CAP_PROP_POS_FRAMES);}
+double VideoReader::fps() const { return hnd.get(CV_CAP_PROP_FPS); }
+unsigned int VideoReader::width() const { return hnd.get(CV_CAP_PROP_FRAME_WIDTH); }
+unsigned int VideoReader::height() const { return hnd.get(CV_CAP_PROP_FRAME_HEIGHT); }
+unsigned int VideoReader::frames() const { return hnd.get(CV_CAP_PROP_FRAME_COUNT); }
+unsigned int VideoReader::frame() const { return hnd.get(CV_CAP_PROP_POS_FRAMES); }
 
-
-VideoReader& VideoReader::operator >> (cv::Mat& matrix) {
+VideoReader &VideoReader::operator>>(cv::Mat &matrix) {
   hnd >> matrix;
   return *this;
 }
-
 
 VideoWriter::VideoWriter(std::string fn, const int width, const int height, const float fps) {
   codec = CV_FOURCC('m', 'p', '4', 'v');
@@ -38,7 +47,7 @@ VideoWriter::VideoWriter(std::string fn, const int width, const int height, cons
   hnd.open(fn, codec, fps, S, true);
 }
 
-VideoWriter& VideoWriter::operator << (const cv::Mat& matrix) {
+VideoWriter &VideoWriter::operator<<(const cv::Mat &matrix) {
   cv::Mat frame;
   matrix.convertTo(frame, CV_8UC3);
   hnd << frame;
